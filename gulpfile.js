4b5c15aeed142eddd1f2e279c62e1d9d01ddb365
valentina-94 <ivanenkovavalya@rambler.ru>
2020-08-28 13:37:29 +0300

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
+let preprocessor = 'sass';
+
 const { src, dest, parallel, series, watch } = require('gulp');
 const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
+const sass = require('gulp-sass');
+const less = require('gulp-less');
+const autoprefixer = require('gulp-autoprefixer');
+const cleancss = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const del = require('del');
 
 function browsersync() {
     browserSync.init({
@@ -22,10 +31,52 @@ function scripts() {
     .pipe(browserSync.stream())
 }
 
+function styles() {
+    return src('app/' + preprocessor + '/main.' + preprocessor + '')
+    .pipe(eval(preprocessor)())
+    .pipe(concat('app.min.css'))
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid:true }))
+    .pipe(cleancss(( { level: { 1: { specialComments: 0 } }/*, format: 'beautify' */} )))
+    .pipe(dest('app/css/'))
+    .pipe(browserSync.stream())
+}
+
+function images() {
+    return src('app/images/src/**/*')
+    .pipe(newer('app/images/dest/'))
+    .pipe(imagemin())
+    .pipe(dest('app/images/dest/'))
+}
+
+function cleanimg() {
+    return del('app/images/dest/**/*', { force: true })
+}
+
+function cleandist() {
+    return del('dist/**/*', { force: true })
+}
+
+function buildcopy() {
+    return src([
+        'app/css/**/*.min.css',
+        'app/js/**/*.min.js',
+        'app/images/dest/**/*',
+        'app/**/*.html',
+        ], { base: 'app' })
+    .pipe(dest('dist'));
+}
+
 function startwatch() {
-    watch(['app/**/*.js', '!app/**/*.min.js'], scripts)
+    watch('app/**/' + preprocessor + '/**/*', styles);
+    watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+    watch('app/**/*.html').on('change', browserSync.reload);
+    watch('app/images/src/**/*', images);
 }
 
 exports.browsersync = browsersync;
 exports.scripts = scripts;
-exports.default = parallel(scripts, browsersync, startwatch);
+exports.styles = styles;
+exports.images = images;
+exports.cleanimg = cleanimg;
+exports.build = series(cleandist, styles, scripts, images, buildcopy)
+exports.default = parallel(scripts, styles, browsersync, startwatch);
